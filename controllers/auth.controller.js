90f8f489581d29259c732770affb646b9a016d8a
saurabh24thakur <saurabh5532u@gmail.com>
2025-07-09 20:18:45 +0530

    signin error removed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,7 +9,10 @@ import bcrypt from "bcryptjs";
 
 export const signUp = async (req, res) => {
     try {
-      const { name, email, password } = req.body;
+      const { username, email, password } = req.body;
+      console.log("Signup hit ✅"); // Debug log
+    console.log("Body:", req.body); // Debug input
+
   
       const existEmail = await User.findOne({ email });
       if (existEmail) {
@@ -23,7 +26,7 @@ export const signUp = async (req, res) => {
       const hashedPassword = await bcrypt.hash(password, 10);
   
       const user = await User.create({
-        name,
+        username,
         email,
         password: hashedPassword,
       });
--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema=mongoose.Schema({
-    name:{
+    username:{
         type:String,
         required:true
     },
@@ -14,12 +14,8 @@ const userSchema=mongoose.Schema({
         type:String,
         required:true
     },
-    assistantName:{
-        type:String,
-    },
-    assistantImage:{
-        type:String,
-    },
+    
+    
     history:[
         {type:String}
     ]
